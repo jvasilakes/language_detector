9f6621cbe16024c55a65f717f9bcc750198490bd
Jake Vasilakes <s1420849@cesena.inf.ed.ac.uk>
2014-10-05 16:33:26 +0100

    Minor style changes.

--- src/gen_random_output.py
+++ src/gen_random_output.py
@@ -1,11 +1,15 @@
+#! /usr/bin/python2
+
+
 import sys
 import numpy as np
 from numpy.random import random_sample
 import random
 
 def gen_random_output(trigram_probs_dict):
-	random_string = np.random.choice(trigram_probs_dict.keys(), 10, trigram_probs_dict.values())
-	print random_string
+	random_string = np.random.choice(trigram_probs_dict.keys(), 50, trigram_probs_dict.values())
+	random_string = ''.join(random_string)
+	return random_string
 
 
 #--------------------------------------------------------#
